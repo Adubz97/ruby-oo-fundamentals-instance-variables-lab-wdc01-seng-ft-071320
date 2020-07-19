8f34489bb03ef9b77ac8caae0d671e3db8b8ab58
Abdullah Albasha <Abdullah.a.albasha@gmail.com>
2020-07-19 15:17:05 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,18 @@
-class Dog
-
-    def name=(dogs_name)
-     @this_dogs_name = dogs_name
+class Dog #class definition
+    #class body
+    #Instance methods 
+    def name=(dogs_name) #writer/setter method
+     @this_dogs_name = dogs_name #defined Instance variable
     end
 
-    def name
-        @this_dogs_name
+    def name # reader/getter method
+        @this_dogs_name #instance variable
     end
 end
-lassie = Dog.new
-lassie = lassie.name
+
+    #Instantiating 
+    lassie = Dog.new #instance of class Dog
+    lassie = lassie.name 
+
+    #print dog name 
+    puts lassie.name #=> lassie 
